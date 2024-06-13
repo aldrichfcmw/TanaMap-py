3e5ce2b680f91786332a0d15b3e55a5f7799e704
aldrichfcmw <aldrichmuktiwibiwo@gmail.com>
2024-06-14 03:10:12 +0700

    feat: add model function

--- gui.py
+++ gui.py
@@ -14,6 +14,7 @@ import requests
 import time
 import exifread
 import json
+# import threading
 
 OUTPUT_PATH = Path(__file__).parent
 ASSETS_PATH = OUTPUT_PATH / Path("assets/")
@@ -24,8 +25,21 @@ def relative_to_assets(path: str) -> Path:
 
 def select_path():
     global input_path
+    filetypes = [
+        ("JPG files", "*.jpg"),
+        ("KML files", "*.kml"),
+    ]
+    jpg_file = filedialog.askopenfilename(title="Select JPG file", filetypes=[("JPG files", "*.jpg")])
+    if jpg_file:
+        log_message(f"Selected JPG file: {jpg_file}")
+    
+    kml_file = filedialog.askopenfilename(title="Select KML file", filetypes=[("KML files", "*.kml")])
+    if kml_file:
+        log_message(f"Selected KML file: {kml_file}")
 
-    input_path = tk.filedialog.askdirectory()
+    input_path = os.path.dirname(jpg_file)
+
+    # input_path = tk.filedialog.askdirectory()
     path_entry.delete(0, tk.END)
     path_entry.insert(0, input_path)
 
@@ -41,63 +55,65 @@ def btn_predict():
         tk.messagebox.showerror(
             title="Empty Fields!", message="Please enter Token.")
         return
-    log_data("Initializing...")
-    time.sleep(3)
-    log_data("Checking Folder Path....")
-    time.sleep(2)
-    if not check_folder(input_path):
-        log_data("Stopping...")
-        time.sleep(1.5)
-        log_data("Stopped! No Image Exist")
-        return
-    time.sleep(1.5)
-    log_data("Checking Server Status....")
-    time.sleep(2)
+    log_message("Initializing...")
+    #time.sleep(3)
+    # log_message("Checking Folder Path....")
+    #time.sleep(2)
+    # if not check_folder(input_path):
+    #     log_message("Stopping...")
+    #     #time.sleep(1.5)
+    #     log_message("Stopped! No Image Exist")
+    #     return
+    #time.sleep(1.5)
+    log_message("Checking Server Status....")
+    #time.sleep(2)
     if not check_server():
-        log_data("Please Chek Your Connection!")
+        log_message("Please Chek Your Connection!")
         return
-    log_data("Checking YoloV5...")
-    time.sleep(3)
-    check_yolo()
-    time.sleep(3)
-    log_data("Checking YoloV5 Model...")
-    time.sleep(3)
+    # log_message("Checking YoloV5...")
+    #time.sleep(3)
+    # check_yolo()
+    #time.sleep(3)
+    log_message("Checking YoloV5 Model...")
+    #time.sleep(3)
     check_model()
-    time.sleep(3)
-    log_data("Processing Detection...")
-    time.sleep(3)
-    predict(input_path)
-    time.sleep(3)
-    log_data("Checking Data...")
-    time.sleep(3)
-    cek_data = check_data(input_path)
-    if not cek_data:
-        log_data("Stopping...")
-        time.sleep(1.5)
-        log_data("Stopped! No Image Exist")
-        return
-    log_data("Uploading Data To Server...")
-    time.sleep(3)
-    upload_data(token,cek_data)
+    #time.sleep(3)
+    # log_message("Processing Detection...")
+    #time.sleep(3)
+    # predict(input_path)
+    #time.sleep(3)
+    # log_message("Checking Data...")
+    #time.sleep(3)
+    # cek_data = check_data(input_path)
+    # if not cek_data:
+        # log_message("Stopping...")
+        #time.sleep(1.5)
+        # log_message("Stopped! No Image Exist")
+        # return
+    # log_message("Uploading Data To Server...")
+    #time.sleep(3)
+    # upload_data(token,cek_data)
 
 
-def log_data(logdata):
-    log_entry.insert(END, logdata + "\n")
-    log_entry.see(END)
+def log_message(message, newline=True):
+    # log_entry.config(state=tk.NORMAL)
+    log_entry.insert(tk.END, message + ('\n' if newline else ''))
+    # log_entry.config(state=tk.DISABLED)
+    # log_entry.yview(tk.END)
+    log_entry.see(tk.END)
     log_entry.update()
 
 def check_folder(folder_path):
     image_files = [file for file in os.listdir(folder_path) if file.endswith(('.jpg', '.png','.JPG', '.PNG'))]
     if image_files:
-        log_data(f"Found {len(image_files)} files JPG/PNG in {folder_path}")
+        log_message(f"Found {len(image_files)} files JPG/PNG in {folder_path}")
         return True
     else:
-        log_data(f"Not Found file JPG/PNG in {folder_path}!")
+        log_message(f"Not Found file JPG/PNG in {folder_path}!")
         return False
-    
 
 def check_server():
-    global status_code
+    global status_code, url
     
     url = "https://tanamap.drik.my.id"  # Ganti dengan URL server yang ingin diperiksa
     try:
@@ -108,83 +124,92 @@ def check_server():
             canvas.itemconfig(image_6, state="hidden")
             canvas.itemconfig(image_5, state="normal")  # Menampilkan gambar 5
             canvas.itemconfig(image_4, state="hidden")  # Sembunyikan gambar 4
-            log_data(f"Server {url} is up and running")
+            log_message(f"Server {url} is up and running")
             return True
         else:
             canvas.itemconfig(status_entry, text=status_code)
             canvas.itemconfig(image_6, state="hidden")
             canvas.itemconfig(image_4, state="normal")  # Menampilkan gambar 4
             canvas.itemconfig(image_5, state="hidden")  # Sembunyikan gambar 5
-            log_data(f"Server {url} is down. Status code: {response.status_code}")
+            log_message(f"Server {url} is down. Status code: {response.status_code}")
             return False
     except requests.ConnectionError:
         canvas.itemconfig(status_entry, text="?")
         canvas.itemconfig(image_6, state="hidden")
         canvas.itemconfig(image_4, state="normal")  # Menampilkan gambar 4
         canvas.itemconfig(image_5, state="hidden")  # Sembunyikan gambar 5
-        log_data("Failed to connect to the server")
+        log_message("Failed to connect to the server")
         return False
 
 def check_yolo():
     directory_path = os.path.dirname(__file__)
     folder_name = os.path.join(directory_path,"yolov5")
     if os.path.exists(folder_name):
-        log_data(f"Yolov5 is exists, Updating...")
+        log_message(f"Yolov5 is exists, Updating...")
         command = ["git", "pull"]
     else:
-        log_data(f"Yolov5 doesn't exist!")
-        log_data(f"Downloading yolov5....")
+        log_message(f"Yolov5 doesn't exist!")
+        log_message(f"Downloading yolov5....")
         command = ["git", "clone", "https://github.com/ultralytics/yolov5.git"]
         sub_command(command,directory_path)
-    log_data("Yolov5 berhasil diupdate")
+    log_message("Yolov5 berhasil diupdate")
     
 def sub_command(command,directory_path):
     import subprocess
     log_entry.insert(tk.END, command)
-    log_data("")
+    log_message("")
     process = subprocess.Popen(command, cwd=directory_path, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, universal_newlines=True)
     while True:
         output = process.stdout.readline()
         if output == '' and process.poll() is not None:
             break
         if output:
-            log_data(output.strip())
+            log_message(output.strip())
             # print(output.strip())
 
 def check_model():
     directory_path = os.path.dirname(__file__)
-    destination_directory = os.path.join(directory_path,"yolov5", "runs", "models")
+    destination_directory = os.path.join(directory_path, "yolov5", "runs", "models")
+    
     if os.path.exists(destination_directory):
-        log_data("Model directory is exist")
+        log_message("Model directory exists")
     else:
-        log_data("Model directory is doesn't exist!")
+        log_message("Model directory doesn't exist! Creating...")
         os.makedirs(destination_directory, exist_ok=True)
 
     save_path = os.path.join(destination_directory, 'best.pt')
+    
     if os.path.exists(save_path):
-        log_data(f"Model file is exist")
+        log_message("Model file exists")
     else:
-        log_data(f"Model file doesn't exist")
-        log_data(f"Downloading Model...")
-        response = requests.get("http://cdn.drik.my.id/files/best.pt", stream=True)
-        total_size = int(response.headers.get('content-length', 0))
-        with open(save_path, 'wb') as file:
-            chunk_size = 1024  # Ukuran chunk yang ingin Anda gunakan
-            downloaded = 0
-            for data in response.iter_content(chunk_size=chunk_size):
-                file.write(data)
-                downloaded += len(data)
-                percent_done = (downloaded / total_size) * 100
-                # print(f"Downloaded: {percent_done:.2f}%\r", end='', flush=True)
-                log_entry.insert(tk.END, "Download Progress: {:.2f}%\n".format(percent_done))
+        log_message("Model file doesn't exist. Downloading...")
+        url = "http://tanamap.drik.my.id/model/best.pt"  # Ganti dengan URL yang sesuai
+        download_model(url,save_path)
+        # threading.Thread(target=download_model, args=(url, save_path)).start()
+
+def download_model(url, save_path):
+    response = requests.get(url, stream=True)
+    total_size = int(response.headers.get('content-length', 0))
+    chunk_size = 1024  # Ukuran chunk yang ingin Anda gunakan
+
+    with open(save_path, 'wb') as file:
+        downloaded = 0
+        log_message("Downloading: [", newline=False)
+        last_printed_progress = 0
+        for data in response.iter_content(chunk_size=chunk_size):
+            file.write(data)
+            downloaded += len(data)
+            percent_done = (downloaded / total_size) * 100
+            print(f"Downloaded: {percent_done:.2f}%\r", end='', flush=True)
+            if int(percent_done) // 5 > last_printed_progress:
+                last_printed_progress = int(percent_done) // 5
+                log_entry.insert(tk.END, "#")
                 log_entry.see(tk.END)
                 log_entry.update()
-        # entry_1.insert(tk.END, "File berhasil diunduh dan disimpan di: {}\n".format(save_path))
-        log_entry.insert(tk.END, "File successfully downloaded \n")
-        log_entry.see(tk.END)
-        log_entry.update()
-
-    # print("\nDownload selesai. File disimpan di:", save_path)
+            # if(percent_done)
+            # log_entry.insert(tk.END, "#")
+        log_message("]", newline=False)
+    log_message("\nFile successfully downloaded")
 
 def predict(input_path):
     directory_path = os.path.dirname(__file__)
@@ -200,7 +225,7 @@ def predict(input_path):
         "--source", input_path
     ]
     sub_command(command,folder_name)
-    log_data("Detection Complete")
+    log_message("Detection Complete")
 
 def dms_to_decimal(dms):
     return dms[0].num / dms[0].den + dms[1].num / (dms[1].den * 60) + dms[2].num / (dms[2].den * 3600)
@@ -233,9 +258,9 @@ def check_data(input_path):
     folders.sort(key=lambda x: os.path.getctime(os.path.join(directory_path, x)), reverse=True)
     latest_folder = os.path.join(directory_path, folders[0])
     if not check_folder(latest_folder):
-        log_data("Stopping...")
-        time.sleep(1.5)
-        log_data("Stopped! No Image Exist")
+        log_message("Stopping...")
+        #time.sleep(1.5)
+        log_message("Stopped! No Image Exist")
         return
     image_list = [file for file in os.listdir(latest_folder) if file.endswith(('.jpg', '.png','.JPG', '.PNG'))]
     gps_data = read_gps_data(input_path)
@@ -243,7 +268,7 @@ def check_data(input_path):
     for image_filename in image_list:
         if image_filename in gps_data:
             gps_info = gps_data[image_filename]
-            # log_data(f"Nama Gambar: {image_filename}, Latitude {gps_info['latitude']}, Longitude {gps_info['longitude']}")
+            # log_message(f"Nama Gambar: {image_filename}, Latitude {gps_info['latitude']}, Longitude {gps_info['longitude']}")
             latitude = str(gps_info['latitude'])
             longitude = str(gps_info['longitude'])
             output_data.append({
@@ -257,7 +282,7 @@ def check_data(input_path):
     log_entry.insert(tk.END, json.dumps(output_data, indent=4))
     log_entry.see(tk.END)
     log_entry.update()
-    log_data("")
+    log_message("")
     return output_data
 
 
@@ -269,9 +294,9 @@ def upload_data(token_bearer,output_data):
     response = requests.post(url, json=output_data, headers=headers)
 
     if response.status_code == 200:
-        log_data("Data is successfully sent to the server")
+        log_message("Data is successfully sent to the server")
     else:
-        log_data(f"Failed to send data to the server. Status code:{response.status_code}" )
+        log_message(f"Failed to send data to the server. Status code:{response.status_code}" )
     return response.status_code
 
 window = Tk()
@@ -487,7 +512,7 @@ canvas.create_text(
     40.0,
     130.0,
     anchor="nw",
-    text="Input Directory",
+    text="Input File",
     fill="#566A7F",
     font=("Public Sans", 12 * -1)
 )
